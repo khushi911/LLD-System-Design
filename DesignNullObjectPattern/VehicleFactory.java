f7a19e671223f828227f39275aaac17e16cc1a19
khushi911 <67455530+khushi911@users.noreply.github.com>
2025-06-24 14:42:11 +0530

    Add files via upload

---
+++ DesignNullObjectPattern/VehicleFactory.java
@@ -0,0 +1,12 @@
+public class VehicleFactory {
+    static Vehicle getVehicleObject(String typeOfVehicle){
+
+        if("Car".equals(typeOfVehicle)) {
+
+            return new Car();
+        }
+
+        return new NullVehicle();
+    }
+
+}
